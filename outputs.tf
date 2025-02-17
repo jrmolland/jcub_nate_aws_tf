2eebb4f2afabc7da7c413753c488299e890dc7ec
jcub <jcub@jcub.jcub>
2025-02-16 22:08:40 -0500

    Added 'public_dns' output in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -19,3 +19,7 @@ output "bastion_host_private_ip" {
 output "bastion_host_public_ip" {
   value = aws_eip.bastion_eip.public_ip
 }
+
+output "public_dns" {
+  value = aws_instance.bastion_host.public_dns
+}
