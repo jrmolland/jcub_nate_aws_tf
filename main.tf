4bc5859843621d0944ccae6e75336ba94e6f3e93
jcub <jcub@jcub.jcub>
2025-02-27 21:03:54 -0500

    Upgraded the App Host instance from t2.micro to t3.medium; required to run node.js and OWASP Juice Shop

--- main.tf
+++ main.tf
@@ -84,7 +84,7 @@ resource "aws_instance" "bastion_host" {
 
 resource "aws_instance" "app_host" {
   ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
+  instance_type          = "t3.medium"
   subnet_id              = aws_subnet.private_subnet.id
   vpc_security_group_ids = [aws_security_group.app_host_sg.id]
   key_name = aws_key_pair.app_host_key.key_name
